07d6a6333f909e6d1081ea0a7978c1d5014d0c7a
Da Kim <daeunkim127@gmail.com>
2022-04-11 00:17:41 -0400

    variable added to js

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+var city = $('#city');
+var date = $('#date');
+var weatherIcon = $('#weather-icon');
+var temperature = $('#temperature');
+var humidity = $('#humidity');
+var wind = $('#wind');
+var uvIndex = $('#uv-index');
+var fiveDayImg = $('.fiveDay-img');
+var fiveDayTemp = $('.fiveDay-temp');
+var fiiveDayHumid = $('.fiveDay-humid');
+var cityInput = $('#city-input');
+var searchBtn = $('#search-btn');
+var cityList = $('.cityList')
+
+var apiKey = '7b109f561dbce00a44f73faa03e0edee';
+
